77bac79c8d4956537405ce2e55ec634875ff823b
nodsup-halnux <le.zigg@gmail.com>
2024-02-13 21:26:02 +0000

    Got licker example running

--- Lick_IPC/licker/client/licker.py
+++ Lick_IPC/licker/client/licker.py
@@ -1,28 +1,43 @@
 from noun import *
 import socket
 
+##Decodes data from our connected socket. So From Urbit -> python
 def cue_data(data):
     #likely, offset +5, little-endian
+    #Jam Format:
+    #[1B: version][4B: size of jam in bytes][nB: jammed data]
     x = cue(int.from_bytes(data[5:], 'little'))
+    print("cue_data output:")
     mark = intbytes(x.head).decode()
+    print(mark)
     noun = x.tail
+    print(noun)
+    print("---")
+
     return (mark,noun)
 
+##Encodes our response for socket. So From Python -> Urbit
 def jam_result(mark, msg):
     mark = int.from_bytes(mark.encode(), 'little')
     noun = int.from_bytes(msg.encode(), 'little')
     return intbytes(jam(Cell(mark, noun)))
 
+#Puts everything together and forms our response frame
+#to be written to the socket file.
 def make_output(jammed):
     length = len(jammed).to_bytes(4, 'little')
+    print("length:" + str(length))
     version = (0).to_bytes(1, 'little')
+    print("version:" + str(version))
     return version+length+jammed
 
 #so sockets are just files in our pier.
 # this makes sense, as everything is a file in unix.
-sock_path = '/home/user/zod/.urb/dev/licker/licker.sock'
+sock_path = '/home/user/Documents/CodeProjects/AdvancedHoon/med/.urb/dev/licker/licker.sock'
+
 ## Template library call
 sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
 ## the socket connection just points to our file we made.
 sock.connect(sock_path)
 
@@ -36,11 +51,11 @@ while True:
     except TimeoutError:
         pass
 
-    # If we pull something other than a noun mark
+    # End loop if we don't pull a noun mark
     if (mark != 'noun'):
       pass
 
-    # Else, we decode
+    # If we did, start decoding.
     msg = intbytes(noun).decode()
     # Look for ping
     if (msg != 'ping'):
